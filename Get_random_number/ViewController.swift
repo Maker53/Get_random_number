44b8245c092963e3d8358787a5128c85a4d32203
Shalgin Stanislav <s.shalgin@gmail.com>
2021-12-10 07:36:11 +0300

    Initial commit

---
+++ Get_random_number/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Get_random_number
+//
+//  Created by Станислав on 10.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
